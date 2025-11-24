f9a94b718f8660c93bdf179515f695a4fa77dc01
Tobias Recker <Recker@match.uni-hannover.de>
2025-11-24 17:31:35 +0100

    feat: add button for Laser Profile Controller and update status handling in ROS interface

--- print_gui/scripts/ros_interface.py
+++ print_gui/scripts/ros_interface.py
@@ -229,12 +229,15 @@ class ROSInterface:
         # --- Parser + Keyence status ---
         mir=self.is_ros_node_running("/retrieve_and_publish_mir_path"); ur=self.is_ros_node_running("/retrieve_and_publish_ur_path")
         key=self.is_ros_node_running("/keyence_ljx_profile_node"); tgt=self.is_ros_node_running("/target_broadcaster")
+        laser=self.is_ros_node_running("/profile_orthogonal_controller"); mocap=self.is_ros_node_running("/qualisys")
 
         # --- Button coloring ---
         self.gui.btn_parse_mir.setStyleSheet("background-color: lightgreen;" if mir else "background-color: lightgray;") if hasattr(self.gui,"btn_parse_mir") else None
         self.gui.btn_parse_ur.setStyleSheet("background-color: lightgreen;" if ur else "background-color: lightgray;") if hasattr(self.gui,"btn_parse_ur") else None
         self.gui.btn_keyence.setStyleSheet("background-color: lightgreen;" if key else "background-color: lightgray;") if hasattr(self.gui,"btn_keyence") else None
         self.gui.btn_target_broadcaster.setStyleSheet("background-color: lightgreen;" if tgt else "background-color: lightgray;") if hasattr(self.gui,"btn_target_broadcaster") else None
+        self.gui.btn_laser_ctrl.setStyleSheet("background-color: lightgreen;" if laser else "background-color: lightgray;") if hasattr(self.gui,"btn_laser_ctrl") else None
+        self.gui.btn_mocap.setStyleSheet("background-color: lightgreen;" if mocap else "background-color: lightgray;") if hasattr(self.gui,"btn_mocap") else None
 
         # --- Auto-start target broadcaster ---
         if mir and ur and not tgt: 
