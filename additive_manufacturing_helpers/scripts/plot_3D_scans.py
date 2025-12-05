fe9747112897c03939cdca962daca2971ec34376
Tobias Recker <Recker@match.uni-hannover.de>
2025-12-05 13:30:24 +0100

    update

--- additive_manufacturing_helpers/scripts/plot_3D_scans.py
+++ additive_manufacturing_helpers/scripts/plot_3D_scans.py
@@ -5,30 +5,56 @@ import matplotlib.pyplot as plt
 import sensor_msgs.point_cloud2 as pc2
 
 # ----------------- CONFIG -----------------
-bagfile = "record_20251205_112954_MuR.bag"
+bagfile = "record_20251205_130609_MuR.bag"
 scan_topic_pc2 = "/profiles"
+output_ply = "scans_export.ply"
 # -------------------------------------------
 
-points = []
+
+# ----------------- PLY EXPORT -----------------
+def save_ply(filename, points):
+    """Save Nx3 points as ASCII PLY file."""
+    N = points.shape[0]
+    with open(filename, "w") as f:
+        # header
+        f.write("ply\n")
+        f.write("format ascii 1.0\n")
+        f.write(f"element vertex {N}\n")
+        f.write("property float x\n")
+        f.write("property float y\n")
+        f.write("property float z\n")
+        f.write("end_header\n")
+
+        # points
+        for x, y, z in points:
+            f.write(f"{x} {y} {z}\n")
+
+    print(f"[OK] Saved PLY file: {filename}")
+
 
 # ----------------- LOAD BAG -----------------
+points = []
+
 with rosbag.Bag(bagfile, "r") as bag:
     for topic, msg, t in bag.read_messages(topics=[scan_topic_pc2]):
 
-        # Each /profiles message is already a PointCloud2 in map frame
         for p in pc2.read_points(msg, field_names=("x", "y", "z"), skip_nans=True):
             points.append([p[0], p[1], p[2]])
 
 points = np.array(points)
 
+
+# ----------------- EXPORT -----------------
+save_ply(output_ply, points)
+
+
 # ----------------- PLOT -----------------
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(points[::100, 0], points[::100, 1], points[::100, 2], s=1)
-
-ax.set_xlabel("X [m]")
-ax.set_ylabel("Y [m]")
-ax.set_zlabel("Z [m]")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
 
 # Equal axis scaling
 max_range = np.array([
