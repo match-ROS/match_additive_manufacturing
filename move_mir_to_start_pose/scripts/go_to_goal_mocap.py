e6de1b562652bd7c523470a2d5d525ebb7768551
Tobias Recker <Recker@match.uni-hannover.de>
2026-01-29 15:04:46 +0100

    Update trajectory follower parameters and goal position in mocap script
    - Changed the gain parameter in the mir_index_offset_applier node from 1.0 to 0.0 to adjust the index offset application behavior.
    - Updated the goal position coordinates in the go_to_goal_mocap script to new values for improved navigation accuracy.

--- move_mir_to_start_pose/scripts/go_to_goal_mocap.py
+++ move_mir_to_start_pose/scripts/go_to_goal_mocap.py
@@ -12,9 +12,9 @@ class GoToGoal:
         self.pose = None
 
         # Zielposition (anpassen)
-        self.goal_x = rospy.get_param("~goal_x", 51.511452)
-        self.goal_y = rospy.get_param("~goal_y", 42.84794)
-        self.yaw_goal = rospy.get_param("~yaw_goal", -2.51)
+        self.goal_x = rospy.get_param("~goal_x", 51.523826)
+        self.goal_y = rospy.get_param("~goal_y", 43.065081)
+        self.yaw_goal = rospy.get_param("~yaw_goal", -2.4130057)
 
         # Reglerparameter
         self.k_lin = 0.4
